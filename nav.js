4a538921d44d5fb5ee1575c171e1156f3837b4fc
Christian Heilmann <chris.heilmann@gmail.com>
2022-08-04 20:01:21 +0200

    Course materials

---
+++ nav.js
@@ -0,0 +1,26 @@
+let urls = [
+    {u:"mobile-version.html",t: "See the document in a mobile view"},
+    {u:"overlay.html",t: "Remove annoying overlays"},
+    {u:"inspect.html",t: "Inspect the uninspectable"},
+    {u:"redirect.html",t: "Avoid unwanted redirects"},
+    {u:"video.html",t: "Get a simpler video player"},
+    {u:"contextmenu.html",t: "Get back the context menu"},
+    {u:"modes.html",t: "Check the document in different modes"},
+    {u:"all-images.html",t: "Grab all images on the page"}
+];
+
+let current = document.location.href.split('/').pop();
+let nav = document.querySelector("#menu");
+urls.forEach(u => {
+    let li = document.createElement("li");
+    if (u.u !== current) {
+        let a = document.createElement("a");
+        a.href = u.u;
+        a.textContent = u.t;
+        li.appendChild(a);
+    } else {
+        li.classList.add('current');
+        li.textContent = u.t;
+    }
+    nav.appendChild(li);
+});
